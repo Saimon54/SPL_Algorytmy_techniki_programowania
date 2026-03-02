bf5d507f0375d0f7407d90fce9282c02f82e8e2d
Saimon54 <splata54@uczen.tl.krakow.pl>
2026-03-02 19:27:51 +0100

    insertion sort.cpp

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -8,12 +8,12 @@
 using namespace std;
 
 void InsertionSort(int tab[], int n) {
-    for (int x = 1; x < n; x++) {
+    for (int x = 1; x < n; x++) {                // This algorithm starts sorting from the second element (1st index)
         int key = tab[x];
         int j = x - 1;
 
 
-        while (j >= 0 && tab[j] > key) {
+        while (j >= 0 && tab[j] > key) {         // '>' and '<' decides if algorithm sort in ascending or descending order
             tab[j+1] = tab[j];
             j--;
         }
@@ -38,4 +38,5 @@ int main() {
     for (int x = 0; x < n; x++) {
         cout << tab[x] << " ";
     }
+
 }
