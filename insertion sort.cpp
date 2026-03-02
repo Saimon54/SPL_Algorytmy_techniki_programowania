0a879d4c0a7f2d18961134b4db1f5ff9fa1a4e40
Saimon54 <splata54@uczen.tl.krakow.pl>
2026-03-02 19:22:17 +0100

    Add files via upload

---
+++ insertion sort.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Szymon on 01.03.2026.
+//
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void InsertionSort(int tab[], int n) {
+    for (int x = 1; x < n; x++) {
+        int key = tab[x];
+        int j = x - 1;
+
+
+        while (j >= 0 && tab[j] > key) {
+            tab[j+1] = tab[j];
+            j--;
+        }
+
+        tab[j+1] = key;
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    const int n = 10;
+    int tab[n];
+
+    cout << "Array before sorting, filled with pseudorandom elements: " << '\n';
+    for (int x = 0; x < n; x++) {
+        tab[x] = rand() % 201-100;
+        cout << tab[x] << " ";
+    }
+
+    cout << '\n' << '\n' << "Array after Insertion sort: " << '\n';
+    InsertionSort(tab, n);
+    for (int x = 0; x < n; x++) {
+        cout << tab[x] << " ";
+    }
+}
---
+++ selection_sort.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Szymon on 01.03.2026.
+//
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void SelectionSort(int tab[], int n){
+    for (int x = 0; x < n - 1; x++) {
+        int IndexOfLowestElement = x;
+
+        for (int y = x + 1; y < n; y++) {
+            if (tab[y] < tab[IndexOfLowestElement])
+                IndexOfLowestElement = y;
+
+        }
+
+        swap(tab[IndexOfLowestElement], tab[x]);
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    const int n = 10;
+    int tab[n];
+
+    cout << "Array after filling it with pseudorandom elements: " << '\n';
+
+    for (int x = 0; x < n; x++) {   // generating random elements and writing them out
+        tab[x] = rand() % 201-100;  // range -100 to 100
+        cout << tab[x] << " ";
+    }
+
+    SelectionSort(tab, n);
+    cout << '\n' << '\n' << "Array after Selection Sort algorithm: " << '\n';
+    for (int x = 0; x < n; x++) {
+        cout << tab[x] << " ";
+    }
+}
